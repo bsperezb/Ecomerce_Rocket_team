4be21fb48a54e53292dd82be9b77bec0753dc967
brayan <brayanperezbuitrago@gmail.com>
2021-10-04 20:33:16 -0500

    listo para deployment

---
+++ authApp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models.user import User
+from .models.account import Account
+
+admin.site.register(User)
+admin.site.register(Account)
+
+
+# Register your models here.
